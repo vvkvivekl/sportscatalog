06a5fc5824819868208fa6abf1085152a634471c
Vivek Lingayat <vvk.vivek.v@gmail.com>
2018-06-02 00:32:57 +0530

    updated application.py

--- application.py
+++ application.py
@@ -335,6 +335,8 @@ def disconnect():
         return redirect(url_for('showSport'))
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
     app.run()
 
     """
@@ -344,7 +346,6 @@ if __name__ == '__main__':
     httpd.serve_forever()
 
 
-	app.secret_key = 'super_secret_key'
-    app.debug = True
+
     app.run(host='0.0.0.0', port=5000)
 """
