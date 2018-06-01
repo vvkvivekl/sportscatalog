625f4ae5d37fc861b1a61a6bc39336d166a8050e
Vivek Lingayat <vvk.vivek.v@gmail.com>
2018-06-01 23:49:11 +0530

    updated application.py

--- application.py
+++ application.py
@@ -32,34 +32,6 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-class Shortener(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        # A GET request will either be for / (the root path) or for /some-name.
-        # Strip off the / and we have either empty string or a name.
-        name = unquote(self.path[1:])
-
-        if name:
-            if name in memory:
-                # We know that name! Send a redirect to it.
-                self.send_response(303)
-                self.send_header('Location', memory[name])
-                self.end_headers()
-            else:
-                # We don't know that name! Send a 404 error.
-                self.send_response(404)
-                self.send_header('Content-type', 'text/plain; charset=utf-8')
-                self.end_headers()
-                self.wfile.write("I don't know '{}'.".format(name).encode())
-        else:
-            # Root path. Send the form.
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            # List the known associations in the form.
-            known = "\n".join("{} : {}".format(key, memory[key])
-                              for key in sorted(memory.keys()))
-            self.wfile.write(form.format(known).encode())
-
 
 # Create anti-forgery state token
 @app.route('/login')
@@ -370,7 +342,7 @@ def disconnect():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = ThreadHTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address)
     httpd.serve_forever()
 
 """
