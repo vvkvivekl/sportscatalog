a055906ff945ec07e985673bc2ebee3db278fbfc
Vivek Lingayat <vvk.vivek.v@gmail.com>
2018-06-01 22:52:35 +0530

    updated application.py

--- application.py
+++ application.py
@@ -168,7 +168,7 @@ def gdisconnect():
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-	if result['status'] == '200':
+	if result['status']=='200':
 		response = make_response(json.dumps('Successfully disconnected.'), 200)
 		response.headers['Content-Type'] = 'application/json'
 		return response
