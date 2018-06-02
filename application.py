125522f100b0e015445eef9a3696787e29d898ec
Vivek Lingayat <vvk.vivek.v@gmail.com>
2018-06-02 08:05:21 +0530

    updated application.py changes process

--- application.py
+++ application.py
@@ -27,6 +27,12 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 # Create anti-forgery state token
 @app.route('/login')
